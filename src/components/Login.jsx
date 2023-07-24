c714df10fb61f11175b862a90c8ff4d0a3e56337
Sam <samy_ch@outlook.fr>
2023-07-24 18:04:10 +0200

    ✨F01: Add Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const Login = () => {
+	const [inputName, setInputName] = useState('');
+
+	const handleChange = (e) => {
+		setInputName(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		inputName !== '' ? alert(`Bonjour ${inputName}`) : '';
+		setInputName('');
+	};
+
+	return (
+		<div>
+			<h2>Bienvenue chez nous !</h2>
+			<form action="submit" onSubmit={() => handleSubmit(event)}>
+				<label htmlFor="name">Connectez-vous</label>
+				<br />
+				<input
+					id="name"
+					value={inputName}
+					required={true}
+					onChange={() => handleChange(event)}
+					type="text"
+					placeholder="entrer votre prénom..."
+				/>
+				<button>Accédez à votre espace</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
