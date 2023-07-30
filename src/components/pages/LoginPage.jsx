61b77a2e05b09521a938da2066b1f296fce5d5f6
Sam <samy_ch@outlook.fr>
2023-07-30 21:22:11 +0200

    💄 F03: Add styled-components and create personalised theme

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,16 @@
 import LoginForm from '../login/LoginForm';
 
 const LoginPage = () => {
-	return <LoginForm />;
+	return (
+		<body>
+			<h1>
+				CRAZEE
+				<img src="/public/img/F03 logo-orange.png" alt="logo burger" />
+				BURGER
+			</h1>
+			<LoginForm />
+		</body>
+	);
 };
 
 export default LoginPage;
