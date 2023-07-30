0dd607c0533a7b0ba50ac86c83bb19f607d4c05b
Sam <samy_ch@outlook.fr>
2023-07-30 22:58:56 +0200

    💄 F03: Login > add styling

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -12,12 +12,27 @@ const LoginPage = () => {
 };
 
 const LoginPageStyled = styled.div`
-	background-color: red;
 	height: 100vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+
+	::before {
+		content: '';
+		width: 100%;
+		height: 100vh;
+		background: url('/img/F03-burger-background.jpg') rgba(0, 0, 0, 0.7);
+		background-size: cover;
+		background-position: center;
+		background-blend-mode: darken;
+		position: absolute;
+		top: 0;
+		left: 0;
+		left: 0;
+		right: 0;
+		z-index: -1;
+	}
 `;
 
 export default LoginPage;
