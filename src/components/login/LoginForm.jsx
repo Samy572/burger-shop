b2fabb541c370450e1c3fd253661916d0ce62785
Sam <samy_ch@outlook.fr>
2023-07-31 12:57:46 +0200

    💄 F03: LOGIN > LoginForm > button > Add styling

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsPersonCircle } from 'react-icons/bs';
+import { IoChevronForward } from 'react-icons/io5';
 import styled from 'styled-components';
 
 const LoginForm = () => {
@@ -31,9 +32,10 @@ const LoginForm = () => {
 					placeholder="entrer votre prénom..."
 				/>
 			</div>
-			<div className="button-container">
-				<button>Acceder à votre espace</button>
-			</div>
+			<button className="button-with-icon">
+				<span>Acceder à votre espace</span>
+				<IoChevronForward className="icon" />
+			</button>
 		</LoginFormStyled>
 	);
 };
@@ -82,8 +84,50 @@ const LoginFormStyled = styled.form`
 			font-size: 15px;
 			color: #17161a;
 		}
-		&::placeholder {
+		input:focus::placeholder {
 			color: lightgrey;
+			
+		}
+	}
+	.button-with-icon {
+		width: 100%;
+		border: 1px solid red;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		white-space: nowrap;
+		text-decoration: none;
+		line-height: 1;
+		padding:18px 24px;
+		border-radius: 5px;
+		font-size:15px;
+		font-weight:800;
+		color: white;
+		background-color: #ff9f1b;
+		border : 1px solid #ff9f1b;
+
+		&:hover:not(:disabled) {
+			background-color: #fff;
+			color: #ff9f1b;
+			border: 1px solid #ff9f1b;
+			transition: all 200ms ease-out;
+		}
+		&:active{
+			color#fff;
+			background-color: #ff9f1b;
+			border: 1px solid #ff9f1b;
+		}
+		&:disabled{
+			opacity:0.5;
+			cursor: not-allowed;
+		}
+		.icon{
+			display:flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 15px;
+			margin-left: 10px;
 		}
 	}
 `;
