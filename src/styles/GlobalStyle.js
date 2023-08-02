7c0e316190ce206c8ee97c54a4a1898ce47a15df
Sam <samy_ch@outlook.fr>
2023-08-02 11:05:06 +0200

    ♻️ F04: GlobalStyle change font

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -5,7 +5,9 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: "Open Sans", sans-serif;
+  }
+  #root{
+     font-family: "Open Sans", sans-serif;
   }
   a{
     text-decoration: none;
