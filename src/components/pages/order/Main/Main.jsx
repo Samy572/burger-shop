cef7d87757014073a052cabb099cfcc3ff0b0033
Sam <samy_ch@outlook.fr>
2023-08-04 22:25:00 +0200

    🚧 F05: OrderPage > Menu > Add grid layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -1,26 +1,26 @@
 import styled from 'styled-components';
 import { theme } from '../../../../theme';
-import Basket from '../Main/Basket';
+// import Basket from '../Main/Basket';
 import Menu from '../Main/Menu';
 
 export default function Main() {
 	return (
 		<MainStyled>
-			<Basket />
+			{/* <Basket /> */}
 			<Menu />
 		</MainStyled>
 	);
 }
 
 const MainStyled = styled.div`
-	height: 95vh;
+	flex: 1;
 	background-color: ${theme.colors.white};
 	border-bottom-right-radius: ${theme.borderRadius.extraRound};
 	border-bottom-left-radius: ${theme.borderRadius.extraRound};
 	box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 	display: grid;
-	grid-template-columns: 25% 1fr;
+	grid-template-columns: 1fr;
 
-	.basket {
-	}
+	/* .basket {
+	} */
 `;
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { fakeMenu2 } from '../../../../data/fakeMenu';
 
 export default function Menu() {
-	return <MenuStyled>Menu</MenuStyled>;
+	const [menu, setmenu] = useState(fakeMenu2);
+
+	return (
+		<MenuStyled>
+			{menu.map((produit, i) => {
+				return (
+					<div key={i} className="produit">
+						{produit.title}
+					</div>
+				);
+			})}
+		</MenuStyled>
+	);
 }
 
 const MenuStyled = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-row-gap: 60px;
 	background-color: pink;
+	overflow-y: scroll;
+	padding: 50px 50px 150px;
+	justify-items: center;
+	flex-wrap: wrap;
+	.produit {
+		background-color: red;
+		height: 330px;
+		width: 240px;
+	}
 `;
