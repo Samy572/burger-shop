089d2be7f87bc0233d2b7d1e2b0484db1dfb5bc7
Sam <samy_ch@outlook.fr>
2023-08-06 18:26:23 +0200

    💄 F05: OrderPage > Main > Product add style

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -1,16 +1,19 @@
 import styled from 'styled-components';
-
+import { theme } from '../../../../theme';
+import PrimaryButton from '../../../reusable-ui/PrimaryButton';
 export default function Product({ title, imageSource, price }) {
 	return (
 		<ProductStyled className="produit">
 			<div className="image">
 				<img src={imageSource} alt={title} />
 			</div>
-			<div className="info-text">
+			<div className="text-info">
 				<div className="title">{title}</div>
 				<div className="description">
-					<div className="price">{price}</div>
-					<div className="add-button">Ajouter</div>
+					<div className="left-description">{price}</div>
+					<div className="right-description">
+						<PrimaryButton className={'primary-button'} label={'Ajouter'} />
+					</div>
 				</div>
 			</div>
 		</ProductStyled>
@@ -19,25 +22,73 @@ export default function Product({ title, imageSource, price }) {
 
 const ProductStyled = styled.div`
 	display: grid;
+	box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+	background: ${theme.colors.white};
+	border-radius: ${theme.borderRadius.extraRound};
 	grid-template-rows: 65% 1fr;
-	background-color: red;
 	height: 330px;
 	width: 240px;
 	padding: 20px 20px 10px 20px;
 	.image {
-		border: white 1px solid;
 		width: 100%;
 		height: auto;
-	}
-	img {
 		margin-top: 30px;
-		width: 100%;
-		height: 100%;
-		object-fit: contain;
+		margin-bottom: 20px;
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: contain;
+		}
 	}
-	.title {
-	}
-	.info-text {
-		border: solid 1px black;
+	.text-info {
+		display: grid;
+		grid-template-rows: 30% 70%;
+		padding: 5px;
+
+		.title {
+			margin: auto 0;
+			font-size: ${theme.fonts.size.P4};
+			position: relative;
+			bottom: 10px;
+			font-weight: ${theme.fonts.weights.bold};
+			color: ${theme.colors.dark};
+			text-align: left;
+			white-space: nowrap;
+			overflow: hidden;
+			width: 100%;
+			text-overflow: ellipsis;
+			font-family: 'Amatic SC', cursive;
+		}
+
+		.description {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+
+			.left-description {
+				display: flex;
+				justify-content: flex-start;
+				align-items: center;
+				font-weight: ${theme.fonts.weights.medium};
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				font-weight: ${theme.fonts.weights.medium};
+				color: ${theme.colors.primary};
+			}
+
+			.right-description {
+				display: flex;
+				justify-content: flex-end;
+				align-items: center;
+				font-size: ${theme.fonts.size.P1};
+
+				.primary-button {
+					font-size: ${theme.fonts.size.XS};
+					cursor: pointer;
+					padding: 12px;
+				}
+			}
+		}
 	}
 `;
