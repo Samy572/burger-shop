35c473249cb4bda39caff60834e22c3e2ca15ece
Sam <samy_ch@outlook.fr>
2023-08-16 23:06:38 +0200

    🚧 F08 : OrderPage > Admin > AddForm > add inputs layout

--- src/components/pages/order/Main/Admin/addForm/AddForm.jsx
+++ src/components/pages/order/Main/Admin/addForm/AddForm.jsx
@@ -1,5 +1,43 @@
+import styled from 'styled-components';
+
 export default function AddForm() {
-  return (
-    <div>AddForm</div>
-  )
+	return (
+		<AddFormStyled>
+			<div className="image-preview">image-preview</div>
+			<div className="input-fields">
+				<input type="text" placeholder="Nom" />
+				<input type="text" placeholder="Image url" />
+				<input type="text" placeholder="Prix" />
+			</div>
+
+			<button className="submit-button">submit button</button>
+		</AddFormStyled>
+	);
 }
+
+const AddFormStyled = styled.form`
+	height: 100%;
+	border: solid 1px black;
+	padding: 30px 5%;
+	display: grid;
+	grid-template-columns: 1fr 3fr;
+	grid-template-rows: repeat(4, 1fr);
+	.image-preview {
+		background: red;
+		grid-area: 1/1/4/2;
+	}
+
+	.input-fields {
+		display: grid;
+		background: blue;
+		grid-area: 1/2/4/2;
+	}
+
+	.submit-button {
+		display: grid;
+		background: green;
+		grid-area: 4/-2;
+		width: 50%;
+		align-items: center;
+	}
+`;
