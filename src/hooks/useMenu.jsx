68818a19c651e6e515a23fc2fa488526e6093fbd
Sam <samy_ch@outlook.fr>
2023-09-06 18:39:30 +0200

    ♻️ F10 : Refacto Orderpage with custom hook useMenu

---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { fakeMenu } from '../data/fakeMenu';
+import { deepClone } from '../utils/array';
+import { EMPTY_PRODUCT } from '../../enums/product';
+
+export const useMenu = () => {
+	const [menu, setMenu] = useState(fakeMenu.LARGE);
+	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+
+	const handleAdd = (newProduct) => {
+		const menuCopy = deepClone(menu);
+		const menuUpdated = [newProduct, ...menuCopy];
+		setMenu(menuUpdated);
+	};
+
+	const handleDelete = async (idProduct) => {
+		const copyMenu = deepClone(menu);
+		const updateMenu = copyMenu.filter((el) => el.id != idProduct);
+		await setMenu(updateMenu);
+		setProductSelected(EMPTY_PRODUCT);
+	};
+
+	const handleEdit = (productBeingUpdate) => {
+		// Copie du state avec un deep clone
+		const menuCopy = deepClone(menu);
+		// Manip de la copie du state
+		const indexOfProductToEdit = menu.findIndex(
+			(menuProduct) => menuProduct.id === productBeingUpdate.id
+		);
+
+		// Update du state
+		menuCopy[indexOfProductToEdit] = productBeingUpdate;
+		setMenu(menuCopy);
+	};
+
+	const resetMenu = () => {
+		setMenu(fakeMenu.MEDIUM);
+	};
+
+	return { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu };
+};
